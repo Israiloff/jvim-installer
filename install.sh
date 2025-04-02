57bb91eb6908f9ed250f7f96d2ff02aa9b2edaa0
Israiloff <stardustindark@gmail.com>
2025-04-02 18:07:57 +0500

    fixed: ubuntu package install command;

--- install.sh
+++ install.sh
@@ -8,8 +8,25 @@ PYTHON_VERSION=3
 TIMEZONE=Asia/Tashkent
 
 # Update system and install dependencies
-apt update && apt upgrade
-apt add --no-cache tzdata bash zip unzip curl openjdk$JDK_VERSION maven git npm yarn neovim zsh build-base libc6-compat gcc g++ make cargo
+apt update -y
+apt upgrade -y
+apt install -y tzdata
+apt install -y zip
+apt install -y unzip
+apt install -y curl
+apt install -y openjdk$JDK_VERSION
+apt install -y maven
+apt install -y git
+apt install -y npm
+apt install -y yarn
+apt install -y neovim
+apt install -y zsh
+apt install -y build-base
+apt install -y libc6-compat
+apt install -y gcc
+apt install -y g++
+apt install -y make
+apt install -y cargo
 
 # Set system timezone
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
@@ -21,7 +38,7 @@ chsh -s /bin/zsh
 # Install and configure Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="half-life"/g' ~/.zshrc
-apt add --no-cache zsh-vcs
+apt install -y zsh-vcs
 
 echo "export SHELL=/bin/zsh" >> $HOME/.zshrc
 
@@ -37,9 +54,9 @@ nvim --headless "+Lazy! sync" +qa
 cd $HOME/.local/share/nvim/lazy/markdown-preview.nvim && yarn install
 
 # Setup ZSH syntax highlighting
-apt add --no-cache zsh-syntax-highlighting
+apt install -y zsh-syntax-highlighting
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
 
 # Setup ZSH autosuggestions
-apt add --no-cache zsh-autosuggestions
+apt install -y zsh-autosuggestions
 echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
