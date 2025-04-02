533134c058521ec187f56f536ee6a956a03d8d72
Israiloff <stardustindark@gmail.com>
2025-04-02 17:50:29 +0500

    fixed: package manager to apt;

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ PYTHON_VERSION=3
 TIMEZONE=Asia/Tashkent
 
 # Update system and install dependencies
-apk update && apk upgrade
-apk add --no-cache tzdata bash zip unzip curl openjdk$JDK_VERSION maven git npm yarn neovim zsh build-base libc6-compat gcc g++ make cargo
+apt update && apt upgrade
+apt add --no-cache tzdata bash zip unzip curl openjdk$JDK_VERSION maven git npm yarn neovim zsh build-base libc6-compat gcc g++ make cargo
 
 # Set system timezone
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
@@ -21,7 +21,7 @@ chsh -s /bin/zsh
 # Install and configure Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="half-life"/g' ~/.zshrc
-apk add --no-cache zsh-vcs
+apt add --no-cache zsh-vcs
 
 echo "export SHELL=/bin/zsh" >> $HOME/.zshrc
 
@@ -37,9 +37,9 @@ nvim --headless "+Lazy! sync" +qa
 cd $HOME/.local/share/nvim/lazy/markdown-preview.nvim && yarn install
 
 # Setup ZSH syntax highlighting
-apk add --no-cache zsh-syntax-highlighting
+apt add --no-cache zsh-syntax-highlighting
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
 
 # Setup ZSH autosuggestions
-apk add --no-cache zsh-autosuggestions
+apt add --no-cache zsh-autosuggestions
 echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
