48a80f00a676eb1fe41f68a32a50462279e33e42
Israiloff <stardustindark@gmail.com>
2025-05-12 11:29:00 +0500

    fixed: configs for debian 12 compatibility;

--- install.sh
+++ install.sh
@@ -1,57 +1,47 @@
 #!/bin/bash
 
-set -e  # Exit on error
-
-# Define variables
-JDK_VERSION=21
-PYTHON_VERSION=3
 TIMEZONE=Asia/Tashkent
-
-# Update system and install dependencies
-apt update -y
-apt upgrade -y
-apt install -y tzdata
-apt install -y zip
-apt install -y unzip
-apt install -y curl
-apt install -y openjdk-$JDK_VERSION-jdk
-apt install -y maven
-apt install -y git
-apt install -y npm
-apt install -y yarn
-apt install -y neovim
-apt install -y zsh
-apt install -y build-base
-apt install -y libc6-compat
-apt install -y gcc
-apt install -y g++
-apt install -y make
-apt install -y cargo
 
 # Set system timezone
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 echo "$TIMEZONE" > /etc/timezone
 
-# Change default shell to ZSH
-chsh -s /bin/zsh
+sudo apt update
+sudo apt install zsh
+sudo exec zsh
+sudo apt install git
+sudo apt install curl
+sudo apt install gcc
+sudo apt install unzip
 
 # Install and configure Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="half-life"/g' ~/.zshrc
-apt install -y zsh-vcs
 
-echo "export SHELL=/bin/zsh" >> $HOME/.zshrc
+# Install latest version of Neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+echo "export PATH=\"$PATH:/opt/nvim-linux-x86_64/bin\"" >> $HOME/.zshrc
 
-# Git configuration
-git config --global alias.pushall '!f() { for remote in $(git remote); do git push "$remote" "$@"; done; }; f'
+#Install Java
+curl -O https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.deb
+sudo dpkg -i jdk-24_linux-x64_bin.deb
 
-# Install JVIM
-mkdir -p ~/.config/nvim
-git clone --depth 1 --branch master https://github.com/Israiloff/jvim.git ~/.config/nvim
-nvim --headless "+Lazy! sync" +qa
+# install Node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
 
-# Repair Markdown Preview
+# Clone Jvim project
+git clone https://github.com/Israiloff/jvim.git $HOME/.config/nvim/
+
+# Repair Markdown Preview plugin
+npm install -g yarn
 cd $HOME/.local/share/nvim/lazy/markdown-preview.nvim && yarn install
+
+# Setup `pushall` alias for Git
+git config --global alias.pushall '!f() { for remote in $(git remote); do git push "$remote" "$@"; done; }; f'
 
 # Setup ZSH syntax highlighting
 apt install -y zsh-syntax-highlighting
@@ -60,3 +50,6 @@ echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlight
 # Setup ZSH autosuggestions
 apt install -y zsh-autosuggestions
 echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
+
+# Synchronize Lazy.nvim plugins
+nvim --headless "+Lazy! sync" +qa
