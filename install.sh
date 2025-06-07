7e8d124e70ea518b002408f7c05c297cc7c17059
Israiloff <stardustindark@gmail.com>
2025-06-07 11:36:33 +0000

    fixed: env extraction;

--- install.sh
+++ install.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 
-TIMEZONE=Asia/Tashkent
+set -e
+
+# Load .env
+if [ -f ".env" ]; then
+  export $(grep -v '^#' .env | xargs)
+else
+  echo ".env file not found!"
+  exit 1
+fi
 
 # Set system timezone
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-echo "$TIMEZONE" > /etc/timezone
+sudo ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+echo "$TIMEZONE" | sudo tee /etc/timezone
 
 # Update package list and install necessary packages
 sudo apt update
-sudo apt install git
-sudo apt install zsh
-sudo apt install curl
-sudo apt install gcc
-sudo apt install unzip
+sudo apt install -y git zsh curl gcc unzip
 
-# Install Neovim itself
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz\nsudo rm -rf /opt/nvim\nsudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-echo "export PATH=\"$PATH:/opt/nvim-linux-x86_64/bin\"" >> $HOME/.zshrc
+# Install Neovim
+curl -LO "https://github.com/neovim/neovim/releases/${NEOVIM_VERSION}/download/nvim-linux-x86_64.tar.gz"
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.zshrc"
 
-# Download and install the latest version of Java (JDK 24)
-curl -O https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.deb
-sudo dpkg -i jdk-24_linux-x64_bin.deb
+# Install Java (JDK)
+curl -O "https://download.oracle.com/java/${JAVA_VERSION}/latest/jdk-${JAVA_VERSION}_linux-x64_bin.deb"
+sudo dpkg -i "jdk-${JAVA_VERSION}_linux-x64_bin.deb"
 
-# Install NVM (Node Version Manager)
+# Install NVM and Node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-# Install Node.js version 22 using NVM
-nvm install 22
+export NVM_DIR="$HOME/.nvm"
+# shellcheck disable=SC1090
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install "$NODE_VERSION"
 
-# Install Jvim project
-git clone https://github.com/Israiloff/jvim.git $HOME/.config/nvim/
+# Install Jvim
+git clone https://github.com/Israiloff/jvim.git "$HOME/.config/nvim/"
 
-# Fixing markdown-preview.nvim dependency issues
+# Fix markdown-preview.nvim dependency
 npm install -g yarn
-cd $HOME/.local/share/nvim/lazy/markdown-preview.nvim && yarn install
+cd "$HOME/.local/share/nvim/lazy/markdown-preview.nvim" && yarn install
 
 # Set up `pushall` alias for git
-git config --global alias.pushall '!f() { for remote in $(git remote); do git push "$remote" "$@"; done; }; f'
+git config --global alias.pushall 
