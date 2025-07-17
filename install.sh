b82f94b49e0535c5d54f118108de27189d1d5439
Israiloff <stardustindark@gmail.com>
2025-07-17 19:16:18 +0500

    fixed: apt commands' confirmation related issues; removed: zsh installation commands;

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install curl
+# Update package list and install curl
+sudo apt update -y
+sudo apt install -y curl
 
 # Download the .env file
 curl -O https://raw.githubusercontent.com/Israiloff/jvim-installer/master/.env
@@ -20,15 +21,9 @@ fi
 sudo ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 
 # Install necessary packages
-sudo apt install zsh
-exec zsh
-sudo apt install git
-sudo apt install gcc
-sudo apt install unzip
-
-# Install and configure Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="half-life"/g' ~/.zshrc
+sudo apt install -y git
+sudo apt install -y gcc
+sudo apt install -y unzip
 
 # Install Neovim
 curl -LO "https://github.com/neovim/neovim/releases/${NEOVIM_VERSION}/download/nvim-linux-x86_64.tar.gz"
@@ -50,20 +45,9 @@ nvm install "$NODE_VERSION"
 # Install Jvim
 git clone https://github.com/Israiloff/jvim.git "$HOME/.config/nvim/"
 
-# Setup ZSH syntax highlighting
-sudo apt install -y zsh-syntax-highlighting
-echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
-
-# Setup ZSH autosuggestions
-sudo apt install -y zsh-autosuggestions
-echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
-
 # Synchronize Lazy.nvim plugins
 nvim --headless "+Lazy! sync" +qa
 
 # Fix markdown-preview.nvim dependency
 npm install -g yarn
 cd "$HOME/.local/share/nvim/lazy/markdown-preview.nvim" && yarn install
-
-# Setup `pushall` alias for Git
-git config --global alias.pushall '!f() { for remote in $(git remote); do git push "$remote" "$@"; done; }; f'
